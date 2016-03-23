14e1f1073757e72a52c88ea959b7db299121dfcc
Paulo Linhares <paulitolinhares@gmail.com>
2016-03-23 11:58:25 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Olá amigos!</title>
+</head>
+<body>
+    <h1><?php echo "Hello, world!" ?></h1>
+</body>
+</html>
+
